0ae1903fad160a9cd1a70c7e711747b79b5fcd9c
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-07-10 17:15:55 -0300

    refactor: desktop menu component

--- src/components/menu-desktop/index.jsx
+++ src/components/menu-desktop/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.scss';
-import MenuOption from '../menu-option';
+import MenuOption from '../menu-options';
+import LogoImage from '../../utils/assets/logo.svg';
 
 export default function MenuDesktop() {
   return (
     <nav className="menu">
       <h3 className="menu__title">Gallery</h3>
-      <image className="menu__image" alt="gallery" />
+      <img className="menu__image" alt="gallery" src={LogoImage}/>
       <p className="menu__subtitle">Bianca Grenzel Severo</p>
       <MenuOption />
     </nav>
