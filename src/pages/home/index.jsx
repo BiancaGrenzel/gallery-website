9161c4ba6d32d1a442dd9c3ee6c3c4df0cae430f
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-07-10 00:37:12 -0300

    feat(WIP): home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './style.scss';
-import { isMobile } from 'react-device-detect';
-import MenuDesktop from '../../components/menu-desktop';
-import MenuMobile from '../../components/menu-mobile';
+import React, { useState } from "react";
+import "./style.scss";
+import { isMobile } from "react-device-detect";
+import MenuDesktop from "../../components/menu-desktop";
+import MenuMobile from "../../components/menu-mobile";
+import UploadImageCard from "../../components/upload-image-card";
+import Modal from "../../components/modal";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
-    <div className="home">
-      {
-            isMobile ? (
-              <MenuMobile />
-            ) : (
-              <MenuDesktop />
-            )
-        }
+    <div className={`home-page ${ isMobile && 'home-page--mobile'}`}>
+      {isMobile ? <MenuMobile /> : <MenuDesktop />}
+      <div className="home-page__content">
+        <UploadImageCard onClick={handleModal} />
+      </div>
+      <Modal isOpen={isModalOpen} onClose={handleModal}>
+        <h3>teste</h3>
+      </Modal>
     </div>
   );
 }
