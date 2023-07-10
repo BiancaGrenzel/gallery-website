e1c28822a99cc455388379cd1bf1010b164ecc30
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-07-10 00:36:50 -0300

    refactor: upload image card component

--- src/components/upload-image-card/index.jsx
+++ src/components/upload-image-card/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import UploadImage from '../../utils/assets/upload.svg';
-import './style.scss';
+import React from "react";
+import UploadImage from "../../utils/assets/upload.svg";
+import "./style.scss";
 
-export default function UploadImageCard() {
+export default function UploadImageCard({ onClick }) {
   return (
-    <div className="upload-card">
+    <div className="upload-card" onClick={onClick}>
       <img className="upload-card__image" src={UploadImage} alt="upload" />
       <p className="upload-card__title">Subir nova imagem</p>
     </div>
